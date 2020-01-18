7f3655a7c89b3d9dc430f63f9c2f15702578df7a
Nikolay Denisevich <n_denisevich@inbox.ru>
2020-01-18 23:27:23 +0300

    Update IntegerExtensions.cs

--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -2,12 +2,71 @@ using System;
 
 namespace Task2
 {
+    /// <summary>
+    /// This class is for finding correct number.
+    /// </summary>
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// This method is for finding gcd.
+        /// </summary>
+        /// <param name="a">
+        /// It is a number a.
+        /// </param>
+        /// <param name="b">
+        /// It is a number b.
+        /// </param>
+        /// <returns>
+        /// It returns gcd.
+        /// </returns>
+        /// <exception cref="System.ArgumentException">
+        /// Two numbers cannot be 0 at the same time.
+        /// </exception>
         public static int GetGcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            long first = a;
+            long second = b;
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+            long gcd = 1;
+            if (first == 0 || second == 0)
+            {
+                gcd = first + second;
+            }
+
+            if (first == 0 && second == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            while (first != 0 & second != 0)
+            {
+                if (first > second)
+                {
+                    first %= second;
+                    if (first == 0)
+                    {
+                        gcd = second;
+                        break;
+                    }
+                }
+                else
+                {
+                    second %= first;
+                    if (second == 0)
+                    {
+                        gcd = first;
+                        break;
+                    }
+                }
+            }
+
+            if (gcd > int.MaxValue)
+            {
+                throw new ArgumentException("Too big return value ");
+            }
+
+            return (int)gcd;
         }
     }
 }
